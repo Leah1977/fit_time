81aac05f2ef4de014ef3ea0d8a22295ab7e8a581
Leah1977 <lh020277@gmail.com>
2021-11-03 17:22:36 +0000

    update changes

--- fit_time/settings.py
+++ fit_time/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--8s^23b+^2^dfoz#3axaq7ya-d8jkc4(!3%o%56v4w+h5)$4y^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
